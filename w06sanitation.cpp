1fceb98a78f9ceb26583bcd56249c73c2c31dd43
Jonathan Hald <jonathanmhald@gmail.com>
2020-10-21 17:39:49 -0600

    added TO DO and stub functions

--- w06sanitation.cpp
+++ w06sanitation.cpp
@@ -14,18 +14,28 @@ string generateQuery(string username, string password)
     return builder.str();
 }
 
+string weakMitigation()
+{
+    //TO DO
+}
+
+string strongMitigation()
+{
+    //TO DO
+}
 int main()
 {
     string username;
     string password;
 
+    //TO DO: add switch statements to select between four attack and if none, weak, or strong mitigation.
     cout << "Please enter your username: ";
     cin >> username;
 
     cout << "Please enter your password: ";
     cin >> password;
 
-    // testing input
+    // Testing input
     cout << "\n" << generateQuery(username, password) << "\n";
     return 0;
 }
