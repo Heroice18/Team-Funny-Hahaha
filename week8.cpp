3343471076f2e18cd79a3d47bae707177d97e8bf
Tyler Peart <tyler.e.peart@gmail.com>
2020-11-03 13:26:28 -0700

    Added arrayIndex and IntegerOverflow. Stated Heap.

--- week8.cpp
+++ week8.cpp
@@ -7,8 +7,12 @@ using namespace std;
  * 2. The array index variable must be reachable through external input.
  * 3. There must not be bounds checking on the array index variable.
  ****************************************/
-void arrayVulnerability(/* feel free to add parameters */)
+void arrayVulnerability(int num)
 {
+   int array[10] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+   cout << "Which Fibbonacchi number would you like to see?\n";
+   cout << "num passed is: " << num << endl;
+   cout << "array[" << num - 1 << "] = " << array[num - 1] << endl;
 }
 
 /**************************************
@@ -18,7 +22,7 @@ void arrayVulnerability(/* feel free to add parameters */)
  *************************************/
 void arrayWorking()
 {
-   arrayVulnerability(/* parameters with non-malicious data */);
+   arrayVulnerability(5);
 }
 
 /**************************************
@@ -31,7 +35,7 @@ void arrayWorking()
  *************************************/
 void arrayExploit()
 {
-   arrayVulnerability(/* parameters with malicious data */);
+   arrayVulnerability(12);
 }
 
 void arcVulnerability()
@@ -85,32 +89,110 @@ void stackExploit()
    stackVulnerability();
 }
 
+/* ***************************************************
+ * Heap Vulnerability
+ * In order for a heap smashing vulnerability to exist, the
+ * following conditions must be met:
+ *  1) There must be two(2) adjacent heap buffers
+ *  2) The first buffer must be reachable through external input
+ *  3) The mechanism to fill the buffer from the external input
+ *     must not correctly check for the buffer size
+ *  4) The second buffer must be released before the first.
+ *  5) The first buffer must be overrun (extended beyond the
+ *     intended limits of the array).
+ * **************************************************/
 void heapVulnerability()
 {
+   //TEST CODE!! NOT COMPLETE!!
+   char * buffer1 = new char[4];
+   char * buffer2 = new char[4];
+   cout << "enter a string: ";
+   cin >> buffer1;
+   cout << "buffer1 = " << buffer1 << endl;
+   cout << "buffer2 = " << buffer2 << endl;
+   delete [] buffer2;
+   cout << "buffer1 post delete= " << buffer1 << endl;
+   delete [] buffer1;
 }
 
+/* ***************************************************
+ * Heap Working
+ * A function that calls heapVulnerability() in a way
+ * that does not yield unexpected behavior.
+ * ***************************************************/
 void heapWorking()
 {
    heapVulnerability();
 }
 
+/* **************************************************
+ * Heap Exploit
+ * For a heap smashing vulnerability to be exploited,
+ * the following steps must occur:
+ *  1) The attacker must provide more data into the outwardly
+ *     facing heap buffer than the buffer is designed to hold.
+ *  2) The attacker must know the layout of the Memory Control Block (MCB)
+ *     (essentially a linked list) residing just after the buffer.
+ *  3) The attacker must provide a new MCB contaning both the location
+ *     of the memory overwrite and the new data to be overwritten.
+ * *************************************************/
 void heapExploit()
 {
    heapVulnerability();
 }
 
-void integerVulnerability()
+/* *******************************************
+ * Integer Vulnerability
+ * In order for an integer overflow to exist the following must exist:
+ *  1) There must be a security check represented by an expression.
+ *  2) The expression must have the potential for overflow
+ *  3) At least one of the numbers used to compute the sentinel must
+ *     be reachable through external input. This sentinel is a variable
+ *     used to make the security decision from the first requirement.
+ * ******************************************/
+void integerVulnerability(size_t numDigits)
 {
+   cout << "Welcome to the HighScore storer!\n";
+   cout << "This program will store your high score as a char array\n";
+   cout << "How many digits are in your high score? (max = 10)\n";
+   cout << "You entered " << numDigits << " digits.\n";
+   //The casting to int creates the vulnerability
+   if ((int)numDigits < 10)
+   {
+      cout << "Test Passed! Thanks for your help!\n";
+   }
+   else
+   {
+      cout << "Test Failed! You entered too many digits.\n";
+   }
+
+   cout << endl; //For extra spacing
+   
 }
 
+/* ********************************************
+ * Integer Working
+ * Calls integerVulnerability() in a way that does not
+ * yield unexpected behavior
+ * ********************************************/
 void integerWorking()
 {
-   integerVulnerability();
+   integerVulnerability(5);
+   integerVulnerability(12);
 }
 
+/* *******************************************
+ * Integer Exploit
+ * To explot an integer overflow, the following must happen:
+ *  1) Provide input, either a buffer size or a single value, that
+ *     is directly or indirectly used in the vulnerable expression.
+ *  2) The input must exceed the valid bounds of the data-type,
+ *     resulting in an overflow or underflow condition.
+ * *******************************************/
 void integerExploit()
 {
-   integerVulnerability();
+   //This number will overflow an integer and appear negative
+   integerVulnerability(3333333333);
 }
 
 void ansiVulnerability()
@@ -159,7 +241,8 @@ int main()
       switch (selection)
       {
       case '1':
-         arrayVulnerability();
+         arrayWorking();
+         arrayExploit();
          break;
       case '2':
          arcVulnerability();
@@ -174,7 +257,8 @@ int main()
          heapVulnerability();
          break;
       case '6':
-         integerVulnerability();
+         integerWorking();
+         integerExploit();
          break;
       case '7':
          ansiVulnerability();
