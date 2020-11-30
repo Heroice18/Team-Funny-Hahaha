3a0f7cd5f7c2539bab24f7bd366e60da35ca4668
Caleb Baird <calebbaird@Calebs-MacBook-Pro-2.local>
2020-11-30 13:00:41 -0700

    Add lab12 starting point.

---
+++ lab12/control.h
@@ -0,0 +1,12 @@
+/***********************************************************************
+ * COMPONENT:
+ *    CONTROL
+ * Author:
+ *    Br. Helfrich, <your name here if you made a change>
+ * Summary:
+ *    This class stores the notion of Bell-LaPadula
+ ************************************************************************/
+
+#pragma once
+
+// you may need to put something here...
