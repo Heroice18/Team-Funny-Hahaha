3ad60c5dc120484620a422eb6a6c5153429ff0db
Brandon Ison <heroice18@gmail.com>
2020-12-03 15:54:23 -0700

    Removed comments in control.cpp

--- lab12/control.cpp
+++ lab12/control.cpp
@@ -1,12 +1,12 @@
 #include "control.h"
 
-// was just copy pasted in. Needs to be moved!
+
 bool securityConditionRead(Control assetControl, Control subjectControl)
 {
    return subjectControl >= assetControl;
 }
 
-// was just copy pasted in. Needs to be moved!
+
 bool securityConditionWrite(Control assetControl, Control subjectControl)
 {
    return subjectControl <= assetControl; // opposite of the Read!
