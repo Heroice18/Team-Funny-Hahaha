8505e47cd7b709ff94b178265e5785a9ce504adf
Caleb Baird <calebbaird@Calebs-MacBook-Pro-2.local>
2020-12-11 01:59:45 -0700

    implement the cipher06 encrypter

--- lab13/cipher06.h
+++ lab13/cipher06.h
@@ -6,6 +6,8 @@
 #ifndef CIPHER06_H
 #define CIPHER06_H
 
+#include <algorithm>
+
 /********************************************************************
  * CLASS
  *******************************************************************/
@@ -14,7 +16,7 @@ class Cipher06 : public Cipher
 public:
    virtual std::string getPseudoAuth()  { return "Thomas Peck"; }
    virtual std::string getCipherName()  { return "Columnar Transposition"; }
-   virtual std::string getEncryptAuth() { return "encrypt author"; }
+   virtual std::string getEncryptAuth() { return "Caleb Baird"; }
    virtual std::string getDecryptAuth() { return "Tyler Peart"; }
 
    /***********************************************************
@@ -48,7 +50,7 @@ public:
       str += "    result += matrix[i]\n";
       str += "  return result\n\n";
 
-      str =  "numbersFromKey(key)\n";
+      str +=  "numbersFromKey(key)\n";
       str += "  vector<pair<char, int>> pairs\n";
       str += "  for 0 <= i < key.length\n"; 
       str += "    pairs[i] = pair(key[i], i)\n";
@@ -58,7 +60,7 @@ public:
       str += "    result.push_back(pairs[i].get<1>())\n";
       str += "  return result\n\n";
 
-      str =  "decrypt(ciphertext, key)\n";
+      str +=  "decrypt(ciphertext, key)\n";
       str += "  numkey = numbersFromKey(key)\n";
       str += "  vector<string> matrix\n";
       str += "  columnSize = ciphertext.length / numkey.length\n";
@@ -79,14 +81,113 @@ public:
    }
 
    /**********************************************************
-    * ENCRYPT
-    * TODO: ADD description
+    * charIndexSorter :: Simple sorting predicate to arrange
+    * the charIndexPairs alphabetically based on the char value.
+    **********************************************************/
+   static bool charIndexSorter(std::pair<char, int> lhs,
+                        std::pair<char, int> rhs)
+   {
+      return std::get<0>(lhs) < std::get<0>(rhs);
+   }
+
+   /**********************************************************
+    * numIndexSorter :: Simple sorting predicate to arrange the
+    * numIndexPairs according to the index value.
+    **********************************************************/
+   static bool numIndexSorter(std::pair<int, int> lhs,
+                    std::pair<int, int> rhs)
+   {
+      return std::get<1>(lhs) < std::get<1>(rhs);
+   }
+
+   /**********************************************************
+    * numbersFromKey :: Takes a key string and returns a vector
+    * of numbers, where each number replaces a letter with its
+    * alphabetical order number based on the other letters.
+    **********************************************************/
+   std::vector<int> numbersFromKey(const std::string & key)
+   {
+      std::vector<int> results;
+
+      int keySize = key.size();
+
+      // Break the key into a vector of the chars paired with the index.
+      std::vector<std::pair<char, int> > charIndexPairs;
+      for (int i = 0; i < keySize; ++i)
+         charIndexPairs.push_back(std::pair<char, int>(key[i], i));
+
+      // Sort the charIndexPairs by the chars
+      std::stable_sort(charIndexPairs.begin(), charIndexPairs.end(),
+                       charIndexSorter);
+
+      // Now replace the chars with the number.
+      std::vector<std::pair<int, int> > numIndexPairs;
+      for (int i = 0; i < keySize; ++i)
+      {
+         std::pair<int, int> p(i + 1, std::get<1>(charIndexPairs[i]));
+         numIndexPairs.push_back(p);
+      }
+
+      std::stable_sort(numIndexPairs.begin(), numIndexPairs.end(),
+                       numIndexSorter);
+
+      for (int i = 0; i < keySize; ++i)
+         results.push_back(std::get<0>(numIndexPairs[i]));
+
+      return results;
+   }
+
+   /**********************************************************
+    * columnNumSorter :: Simple sorting predicate to arrange
+    * the columNumPairs by the num value.
+   **********************************************************/
+   static bool columnNumSorter(std::pair<std::string, int> lhs,
+                                 std::pair<std::string, int> rhs)
+   {
+      return std::get<1>(lhs) < std::get<1>(rhs);
+   }
+
+   /**********************************************************
+    * ENCRYPT :: Construct a matrix as many columns as there
+    * are characters in password. Populate columns with the
+    * characters of plainText. Rearrange the columns of the
+    * matrix to match the alphabetical order of the password.
+    * concatenate columns of matrix to generate ciphertext.
     **********************************************************/
    virtual std::string encrypt(const std::string & plainText,
                                const std::string & password)
    {
-      std::string cipherText = plainText;
-      // TODO - Add your code here
+      std::vector<std::string> matrix;
+      int passwordSize = password.size();
+
+      // Each empty string in this vector is a column of the matrix
+      for (int i = 0; i < passwordSize; ++i)
+         matrix.push_back("");
+
+      // Add plain text to the matrix by appending to the columns
+      for (int i = 0; i < plainText.size(); ++i)
+         matrix[i % passwordSize] += plainText[i];
+
+      // get the keyNumbers to know how to rearrange the columns
+      std::vector<int> keyNumbers = numbersFromKey(password);
+
+      // pair the columns with the keyNumbers so they can be rearranged
+      std::vector<std::pair<std::string, int> > columnNumPairs;
+      for (int i = 0; i < passwordSize; ++i)
+      {
+         std::pair<std::string, int> p(matrix[i], keyNumbers[i]);
+         columnNumPairs.push_back(p);
+      }
+
+      // Rearrange the columns
+      std::stable_sort(columnNumPairs.begin(), columnNumPairs.end(),
+                       columnNumSorter);
+
+      // Concatenate the rearranged columns into one cipherText string.
+      std::string cipherText = "";
+      for (int i = 0; i < passwordSize; ++i)
+         cipherText += std::get<0>(columnNumPairs[i]);
+
       return cipherText;
    }
 
