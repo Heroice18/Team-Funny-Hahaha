10cd480cd5dbfcca60e2845d1167eb474d9ced17
Jonathan Hald <jonathanmhald@gmail.com>
2020-12-11 11:17:07 -0700

    fixed more pseudocode for cipher03

--- lab13/cipher03.h
+++ lab13/cipher03.h
@@ -41,13 +41,14 @@ public:
       // TODO: please format your pseudocode
       // The encrypt pseudocode
       str += "encryption pseudocode\n";
+      str += "size_t len <- cipherText size\n";
       str += "for size_t i -> 0, i < len, increment i\n";
       str += "cipherText[i] -> short int plainText[i] ^ short int password[i]\n";
 
 
       // The decrypt pseudocode
       str += "decryption pseudocode\n";
-      str += "size_t len -> cipherText size\n";
+      str += "size_t len -> plainText size\n";
       str += "for size_t i -> 0, i < len, increment i\n";
       str += "plainText[i] -> short int cipherText[i] ^ short int password[i]\n";
 
